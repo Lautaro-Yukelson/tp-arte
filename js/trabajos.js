a8dd63f1681f99663e8681a7e1f7f7726d023a57
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-07-12 11:56:23 -0300

    Borron y cuenta nueva - Sidebar - Trabajos Bandas git push
    git push
    exit

--- js/trabajos.js
+++ js/trabajos.js
@@ -1,31 +1,25 @@
-let Trabajos = [
-  {
-    id: 0,
-    title: "Slipknot",
-    desc: "Slipknot es una banda estadounidense de metal alternativo formada en 1995. Conocidos por su estética y su enérgica puesta en escena, el grupo es reconocido por usar máscaras y monos en sus actuaciones. Su música combina elementos de metal, nu metal y heavy metal, y se caracteriza por sus riffs pesados, ritmos agresivos y letras introspectivas y emotivas. Con canciones como Duality, Wait and Bleed y Psychosocial, Slipknot ha ganado una base de fans leales y se ha convertido en un referente del metal moderno.",
-    date: "24/2/2022",
-    author: "Iair Steimberg",
-    pic: "./Assets/slipknot.jfif",
-    category: "bandas",
-  },
-  {
-    id: 1,
-    title: "System of a Down",
-    desc: "System of a Down es una banda de rock con un sonido único y una fusión de estilos que incluye rock, metal alternativo y folclore armenio. Sus letras tratan temas sociales y políticos, mientras que su música se caracteriza por ritmos intensos, cambios de tempo y poderosos mensajes. Han alcanzado gran éxito con álbumes como Toxicity y Mezmerize/Hypnotize, y su legado perdura en la escena del rock.",
-    date: "12/5/2021",
-    author: "Iair Steimberg",
-    pic: "./Assets/systemOfADown.jfif",
-    category: "bandas",
-  },
-  {
-    id: 2,
-    title: "Iron Maiden",
-    desc: "Iron Maiden es una icónica banda británica de heavy metal formada en 1975. Reconocidos por su mascota Eddie y sus espectaculares shows en vivo, Iron Maiden ha dejado una huella indeleble en la historia del género. Su música combina melodías épicas, guitarras duales y la potente voz de Bruce Dickinson. Con álbumes como The Number of the Beast y Powerslave, Iron Maiden ha creado himnos como Run to the Hills y Fear of the Dark, estableciéndose como una de las bandas más influyentes y exitosas del heavy metal.",
-    date: "21/10/2027",
-    author: "Iair Steimberg",
-    pic: "./Assets/ironMaiden.webp",
-    category: "bandas",
-  },
-];
+import { Trabajos } from './listas.js';
 
-export { Trabajos };
+function mostrarTrabajos(){
+    console.log(Trabajos);
+    for (let tp of Trabajos) {
+      let container = document.getElementById("cards-container");
+      let card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+        <a href="${tp.link}" target="_BLANK">
+            <div class="card-image">
+                <img src="${tp.img}" alt="">
+            </div>
+            <p class="card-title">${tp.title}</p>
+            <p class="card-body">
+                ${tp.desc}
+            </p>
+            <p class="footer">Por <span class="by-name">${tp.author}</span> - <span class="date">${tp.curso}</span></p>
+        </a>
+      `;
+      container.appendChild(card);
+    }    
+}
+
+mostrarTrabajos();
